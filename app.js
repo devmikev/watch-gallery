40fd5e25eb25871a1ed5fc831372ab517a0a3d45
Mike V <dev.mikev@gmail.com>
2018-07-10 20:14:28 -0400

    add bs4 cards to galleries.ejs

--- app.js
+++ app.js
@@ -5,22 +5,28 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+var galleries = [
+  {name: 'Mike\'s watches', image: 'https://images.unsplash.com/photo-1494858723852-d3cc2477e12c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a66c81d6441ca061f1e4647df4f41e40&auto=format&fit=crop&w=1056&q=80'},
+  {name: 'Vlad\'s watches', image: 'https://images.unsplash.com/photo-1526045431048-f857369baa09?ixlib=rb-0.3.5&s=f26461245bdc7f69ba2f6ea2ba94be54&auto=format&fit=crop&w=1050&q=80'},
+  {name: 'Ilya\'s watches', image: 'https://images.unsplash.com/photo-1488132828189-4e416661b680?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5a16bce80dad1d3afc02a021bd255357&auto=format&fit=crop&w=1350&q=80'}
+]
+
 app.get('/', (req, res) => {
   res.render("landing");
 });
 
 app.get('/galleries', (req, res) => {
-  var galleries = [
-    {name: 'Mike\'s watches', image: 'https://images.unsplash.com/photo-1494858723852-d3cc2477e12c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a66c81d6441ca061f1e4647df4f41e40&auto=format&fit=crop&w=1056&q=80'},
-    {name: 'Vlad\'s watches', image: 'https://images.unsplash.com/photo-1526045431048-f857369baa09?ixlib=rb-0.3.5&s=f26461245bdc7f69ba2f6ea2ba94be54&auto=format&fit=crop&w=1050&q=80'},
-    {name: 'Ilya\'s watches', image: 'https://images.unsplash.com/photo-1455651512878-0ddbb4c4d0a5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=32a7e4edd69408cfe6fce705c5c98592&auto=format&fit=crop&w=675&q=80'}
-  ]
+  
 
   res.render('galleries', {galleries});
 });
 
 app.post('/galleries', (req, res) => {
-  res.send('This is the post route!');
+  var name = req.body.name;
+  var image = req.body.image;
+  var newGallery = {name, image};
+  galleries.push(newGallery);
+  res.redirect('/galleries');
 });
 
 app.get("/galleries/new", (req, res) => {
